732dd82523410070d83872e8f52acd266b9d8328
Felipe Sartori <sartorifelipe@icloud.com>
2023-07-19 10:53:02 -0300

    Changing project to maven

--- hr-worker/src/test/java/com/sarta/hrworker/DemoApplicationTests.java
+++ hr-worker/src/test/java/com/sarta/hrworker/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrWorkerApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
