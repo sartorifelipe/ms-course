5664b6fed3a9feaeb9fceb984390a4ebf1b3cfd0
Felipe Sartori <sartorifelipe@icloud.com>
2023-07-19 11:06:37 -0300

    Feign implementation in previous commit and adjust in import.sql

--- hr-worker/src/main/resources/import.sql
+++ hr-worker/src/main/resources/import.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO tb_worker (name, DAILY_INCOME) VALUES ('Bob', 200.0);
 INSERT INTO tb_worker (name, DAILY_INCOME) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker (name, DAILY_INCOME) VALUES ('Alex', 250.0);
